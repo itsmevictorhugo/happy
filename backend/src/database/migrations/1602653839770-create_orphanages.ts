e87ff26de234c17922e7fb62a7eddeeaf0807e4d
Victor Hugo <csivictorhugo@gmail.com>
2020-10-14 03:32:03 -0300

    backend: modelos orfanatos criado

--- backend/src/database/migrations/1602653839770-create_orphanages.ts
+++ backend/src/database/migrations/1602653839770-create_orphanages.ts
@@ -33,6 +33,10 @@ export class createOrphanages1602653839770 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
