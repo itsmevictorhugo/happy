72f72d5924ccde2514c9da5f27a8caf96f3dccf8
Victor Hugo <csivictorhugo@gmail.com>
2020-10-14 04:32:19 -0300

    backend: listagem orfanatos

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
+  async index(request: Request, response: Response){
+    const orphanagesRepository = getRepository(Orphanage);
+    
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
   async create(request: Request, response: Response) {
     const {
       name,
