4e0f99bd10f6d18850cac435bc8f2628266818c3
Victor Hugo <csivictorhugo@gmail.com>
2020-10-14 04:37:35 -0300

    backend: detalhe orfanato

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ const routes = Router();
 
 
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
